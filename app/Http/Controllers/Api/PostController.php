5d8c1a4d7421838a155bcce8eaa1e3cefe4e56c0
Mario Koco <mario.koco92@gmail.com>
2021-06-29 00:43:12 +0200

    costruito array dell'api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -12,8 +12,19 @@ class PostController extends Controller
     public function index() {
         $posts = Post::all();
 
+        // costruisco l'array per prendere i dati a me necessari
+        $posts_data = [];
+        foreach($posts as $post) {
+            $posts_data[] = [
+                'title' => $post->title,
+                'content' => $post->content,
+                // uso un ternario per verificare che la categoria non sia null
+                'category' => $post->category ? $post->category->name : ''
+            ];
+        }
+
         $data = [
-            'posts' => $posts,
+            'posts' => $posts_data,
             // Per dire all'utente che la chiamata è riuscita
             'success' => true
         ];
