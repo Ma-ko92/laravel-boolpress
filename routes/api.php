a9ccff75a1aa0c9f11f0ccd3d85955cd16599c2b
Mario Koco <mario.koco92@gmail.com>
2021-06-29 20:32:30 +0200

    fix commento

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Rote di esempio
+// Route di esempio
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
